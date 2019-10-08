057f9beaf624c0ae0b5bf7ca820ec8d571946f9b
JensVanAssche <jens.va@hotmail.com>
2019-10-08 18:58:39 +0200

    dropdown click hide

--- src/header/Header.js
+++ src/header/Header.js
@@ -3,63 +3,81 @@ import { Link, NavLink } from 'react-router-dom';
 import './header.scss';
 import logoWhite from 'assets/images/logo_white.png';
 
-function Header() {
-  return (
-    <header>
-      <div className="header-image" style={{'backgroundImage': `url(${process.env.REACT_APP_API_HOST}/header.jpg)`}}>
-        <div className="ui container">
-        <Link to="/"><img src={logoWhite} alt="Logo" /></Link>
+class Header extends React.Component {
+  onMenuClick() {
+    const aaa = document.querySelectorAll('.dropdown .menu');
+    aaa.forEach(e => {
+      e.classList.add('hidden');
+    });
+  }
+
+  onHover() {
+    const aaa = document.querySelectorAll('.dropdown .menu');
+    aaa.forEach(e => {
+      e.classList.remove('hidden');
+    });
+  }
+
+  render() {
+    return (
+      <header>
+        <div className="header-image" style={{'backgroundImage': `url(${process.env.REACT_APP_API_HOST}/header.jpg)`}}>
+          <div className="ui container">
+          <Link to="/"><img src={logoWhite} alt="Logo" /></Link>
+          </div>
         </div>
-      </div>
-      <nav>
-        <ul className="ui container">
-          <li><NavLink to="/" exact={true} >HOME</NavLink></li>
-          <li><NavLink to="/spinnaker">SPINNAKER</NavLink></li>
-          <li>
-            <div className={"ui simple dropdown " +
-            (window.location.pathname.split("/")[1] === "boccia" ? 'aaaaa' : "") +
-            (window.location.pathname.split("/")[1] === "dansen" ? 'aaaaa' : "") +
-            (window.location.pathname.split("/")[1] === "hockey" ? 'aaaaa' : "") +
-            (window.location.pathname.split("/")[1] === "handbal" ? 'aaaaa' : "") +
-            (window.location.pathname.split("/")[1] === "zwemmen" ? 'aaaaa' : "")}>
-              <span>SPORTEN</span>
-              <div className="menu">
-                <Link to="/boccia">BOCCIA</Link>
-                <Link to="/dansen">DANSEN</Link>
-                <Link to="/hockey">HOCKEY</Link>
-                <Link to="/handbal">ROLSTOELHANDBAL</Link>
-                <Link to="/zwemmen">ZWEMMEN</Link>
+        <nav>
+          <ul className="ui container">
+            <li><NavLink to="/" exact={true} >HOME</NavLink></li>
+            <li><NavLink to="/spinnaker">SPINNAKER</NavLink></li>
+            <li>
+              <div className={"ui simple dropdown " +
+              (window.location.pathname.split("/")[1] === "boccia" ? 'aaaaa' : "") +
+              (window.location.pathname.split("/")[1] === "dansen" ? 'aaaaa' : "") +
+              (window.location.pathname.split("/")[1] === "hockey" ? 'aaaaa' : "") +
+              (window.location.pathname.split("/")[1] === "handbal" ? 'aaaaa' : "") +
+              (window.location.pathname.split("/")[1] === "zwemmen" ? 'aaaaa' : "")}
+              onMouseEnter={() => this.onHover()}>
+                <span>SPORTEN</span>
+                <div className="menu" onClick={() => this.onMenuClick()}>
+                  <Link to="/boccia">BOCCIA</Link>
+                  <Link to="/dansen">DANSEN</Link>
+                  <Link to="/hockey">HOCKEY</Link>
+                  <Link to="/handbal">ROLSTOELHANDBAL</Link>
+                  <Link to="/zwemmen">ZWEMMEN</Link>
+                </div>
               </div>
-            </div>
-          </li>
-          <li>
-            <div className="ui simple dropdown">
-              <span>KALENDER</span>
-              <div className="menu">
-                <Link to="/boccia/kalender">BOCCIA KALENDER</Link>
-                <Link to="/hockey/kalender">HOCKEY KALENDER</Link>
-                <Link to="/handbal/kalender">HANDBAL KALENDER</Link>
-                <Link to="/zwemmen/kalender">ZWEMMEN KALENDER</Link>
+            </li>
+            <li>
+              <div className="ui simple dropdown" onMouseEnter={() => this.onHover()}>
+                <span>KALENDER</span>
+                <div className="menu" onClick={() => this.onMenuClick()}>
+                  <Link to="/boccia/kalender">BOCCIA KALENDER</Link>
+                  <Link to="/hockey/kalender">HOCKEY KALENDER</Link>
+                  <Link to="/handbal/kalender">HANDBAL KALENDER</Link>
+                  <Link to="/zwemmen/kalender">ZWEMMEN KALENDER</Link>
+                </div>
               </div>
-            </div>
-          </li>
-          <li>
-            <div className={"ui simple dropdown " +
-            (window.location.pathname.split("/")[1] === "fotos" ? 'aaaaa' : "") +
-            (window.location.pathname.split("/")[1] === "videos" ? 'aaaaa' : "")}>
-              <span>GALLERIJ</span>
-              <div className="menu">
-                <Link to="/fotos">FOTO'S</Link>
-                <Link to="/videos">VIDEO'S</Link>
+            </li>
+            <li>
+              <div className={"ui simple dropdown " +
+              (window.location.pathname.split("/")[1] === "fotos" ? 'aaaaa' : "") +
+              (window.location.pathname.split("/")[1] === "videos" ? 'aaaaa' : "")}
+              onMouseEnter={() => this.onHover()}>
+                <span>GALLERIJ</span>
+                <div className="menu" onClick={() => this.onMenuClick()}>
+                  <Link to="/fotos">FOTO'S</Link>
+                  <Link to="/videos">VIDEO'S</Link>
+                </div>
               </div>
-            </div>
-          </li>
-          <li><NavLink to="/publicaties">PUBLICATIES</NavLink></li>
-          <li><NavLink to="/nieuws">NIEUWS</NavLink></li>
-        </ul>
-      </nav>
-    </header>
-  );
+            </li>
+            <li><NavLink to="/publicaties">PUBLICATIES</NavLink></li>
+            <li><NavLink to="/nieuws">NIEUWS</NavLink></li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
 }
 
 export default Header;
