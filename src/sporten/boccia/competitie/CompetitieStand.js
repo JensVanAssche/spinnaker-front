f69c79057c04f36469e452afeb8dfd3791694bd8
JensVanAssche <jens.va@hotmail.com>
2019-11-15 16:48:34 +0100

    fixes

--- src/sporten/boccia/competitie/CompetitieStand.js
+++ src/sporten/boccia/competitie/CompetitieStand.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 import { selectStandings, selectLoading } from "redux/standings/selectors";
 import { getStandings } from "redux/standings/actions";
 
 class CompetitieStand extends React.Component {
   state = {
-    title: ['Parantee Competitie Stand', 'Scholencompetitie Stand', 'Competitie Nederland Stand'],
-    types: ['parantee', 'scholen', 'boccianederland'],
+    title: [
+      "Parantee Competitie Stand",
+      "Scholencompetitie Stand",
+      "Competitie Nederland Stand"
+    ],
+    types: ["parantee", "scholen", "boccianederland"]
   };
 
   componentDidMount() {
@@ -58,22 +62,23 @@ class CompetitieStand extends React.Component {
                 </div>
               </div>
               <div className="body">
-                {result.scores.length === 0 && (
+                {result.scores && result.scores.length === 0 && (
                   <div className="entry empty">
                     <p>Geen standen in dit tornooi</p>
                   </div>
                 )}
-                {result.scores && result.scores.map(score => (
-                  <div className="entry" key={score.id}>
-                    <div>
-                      <p>{score.name}</p>
+                {result.scores &&
+                  result.scores.map(score => (
+                    <div className="entry" key={score.id}>
+                      <div>
+                        <p>{score.name}</p>
+                      </div>
+                      <div>
+                        <p>{score.points1}</p>
+                        <p>{score.points2}</p>
+                      </div>
                     </div>
-                    <div>
-                      <p>{score.points1}</p>
-                      <p>{score.points2}</p>
-                    </div>
-                  </div>
-                ))}
+                  ))}
               </div>
             </div>
           ))}
@@ -86,7 +91,13 @@ class CompetitieStand extends React.Component {
         <h2>{this.state.title[league]}</h2>
         {data.map(result => (
           <div key={result.id}>
-            <a href={process.env.REACT_APP_API_HOST + result.pdf} target="_blank" rel="noopener noreferrer">{result.title}</a>
+            <a
+              href={process.env.REACT_APP_API_HOST + result.pdf}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {result.title}
+            </a>
           </div>
         ))}
       </div>
@@ -95,7 +106,7 @@ class CompetitieStand extends React.Component {
 }
 
 const mapDispatchToProps = {
-  getStandings,
+  getStandings
 };
 
 const mapStateToProps = state => ({
@@ -103,7 +114,4 @@ const mapStateToProps = state => ({
   loading: selectLoading(state)
 });
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(CompetitieStand);
+export default connect(mapStateToProps, mapDispatchToProps)(CompetitieStand);
